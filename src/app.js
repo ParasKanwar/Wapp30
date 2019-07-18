1d6d3b14590ddd8735a91829d6dc95d64ec3d874
paras <paraskanwar30@gmail.com>
2019-07-18 22:26:13 +0530

    2nd commit

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const hbs = require("hbs");
 const SharedLocation = path.join(__dirname, "../Shared");
 const ViewLocation = path.join(__dirname, "../Templates/Views");
 const partialLocation = path.join(__dirname, "../Templates/Partials");
-
+const port = process.env.PORT || 3000;
 app.set("view engine", "hbs");
 app.set("views", ViewLocation);
 hbs.registerPartials(partialLocation);
 app.use(express.static(SharedLocation));
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", { Title: "HomePage", Name: "Paras" });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(port, () => {
+  console.log("listening on port" + port);
 });
